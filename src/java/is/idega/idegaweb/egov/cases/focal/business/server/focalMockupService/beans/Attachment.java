68a8ce8c75754fae322f55e9e3ff14d1458dde29
Vytautas Civilis <civilis@idega.com>
2007-05-05 15:19:08 +0000

    comment about attachments implementation

--- src/java/is/idega/idegaweb/egov/cases/focal/business/server/focalMockupService/beans/Attachment.java
+++ src/java/is/idega/idegaweb/egov/cases/focal/business/server/focalMockupService/beans/Attachment.java
@@ -2,10 +2,16 @@ package is.idega.idegaweb.egov.cases.focal.business.server.focalMockupService.be
 
 /**
  * 
- * Last modified: $Date: 2007/05/05 13:19:14 $ by $Author: civilis $
+ * <p>Currently file is transfered in soap envelope using base64. That's not too efficient, but whole lot simpler.
+ * If this becomes an issue (e.g. there are lots of files transfered occasionally or they are big), 
+ * try to implement it in effective way.
+ * </p> 
+ * @see http://www.mail-archive.com/axis-user@xml.apache.org/msg08732/Fear_of_Attachments.pdf
+ * 
+ * Last modified: $Date: 2007/05/05 15:19:08 $ by $Author: civilis $
  * 
  * @author <a href="civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class Attachment {
 
