b31e3aec41c6211fc1a7ef43eef4d52e15ac4adb
Aleksandras Skrynikovas <alexis@idega.com>
2007-05-29 07:44:26 +0000

    adding styles and js for handling selecting a project

--- resources/javascript/focal.js
+++ resources/javascript/focal.js
@@ -1,3 +1,9 @@
-function selectFocalCasesRow() {
-	//alert('Boom');
+function selectFocalCasesRow(e) {
+	if (!e) e = window.event;
+	if (!e) return true;
+	if(e.srcElement) {
+		
+	} else if(e.target) {
+		alert(e.target);
+	}
 }
