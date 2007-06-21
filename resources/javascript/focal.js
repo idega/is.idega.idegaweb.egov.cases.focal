2c04c02988caff857da68bbc79655cb79b3cc790
Aleksandras Skrynikovas <alexis@idega.com>
2007-06-21 20:54:29 +0000

    final bug fix from the current list of issues

--- resources/javascript/focal.js
+++ resources/javascript/focal.js
@@ -6,7 +6,7 @@ function selectFocalCasesRow(e) {
 		var newRow = document.getElementById(e);
 		if(newRow != null) {
 			if(newRow.className) {
-				newRow.className = 'selectedProject';
+				newRow.className = 'selectedProject ruled';
 				if(focal_project_row_id != null && focal_project_row_id != e) {
 					var oldRow = document.getElementById(focal_project_row_id);
 					if(oldRow != null) {
