97c4796056815445fb3043bd8c54847b2f6de680
Aleksandras Skrynikovas <alexis@idega.com>
2007-08-21 12:57:27 +0000

    last updates and fixes

--- src/java/is/idega/idegaweb/egov/cases/focal/business/ExportCasesManagementHomeImpl.java
+++ src/java/is/idega/idegaweb/egov/cases/focal/business/ExportCasesManagementHomeImpl.java
@@ -6,17 +6,17 @@ import com.idega.business.IBOHomeImpl;
 
 /**
  * 
- * Last modified: $Date: 2007/05/05 18:47:55 $ by $Author: civilis $
+ * Last modified: $Date: 2007/08/21 12:57:27 $ by $Author: alexis $
  * 
  * @author <a href="civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ExportCasesManagementHomeImpl extends IBOHomeImpl implements ExportCasesManagementHome {
 
 	private static final long serialVersionUID = -8350283595156018241L;
 
 	public Class getBeanInterfaceClass() {
-		return FocalCasesIntegration.class;
+		return ExportCasesManagement.class;
 	}
 
 	public ExportCasesManagement create() throws CreateException {
